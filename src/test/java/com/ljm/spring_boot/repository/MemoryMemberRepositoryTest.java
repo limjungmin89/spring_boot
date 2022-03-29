ac85d33c2a21c2de69c2c4ef3e7d752d21459d48
Jungmin Lim <jungminim@mz.co.kr>
2022-03-29 11:43:47 +0900

    test case commit

---
+++ src/test/java/com/ljm/spring_boot/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.ljm.spring_boot.repository;
+
+import com.ljm.spring_boot.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+
+        member.setName("limjungmin");
+        memberRepository.save(member);
+
+        Member result = memberRepository.findById(member.getId()).get();
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("member1");
+        memberRepository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("member2");
+        memberRepository.save(member2);
+
+        Member result = memberRepository.findByName("member1").get();
+
+        Assertions.assertThat(result).isEqualTo(member1);
+
+    }
+}
