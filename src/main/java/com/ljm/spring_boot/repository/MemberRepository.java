6b1ffb362d4cc13109a3fdfaad3e5d54c763f23b
Jungmin Lim <jungminim@mz.co.kr>
2022-03-29 11:15:28 +0900

    memory version member add

---
+++ src/main/java/com/ljm/spring_boot/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.ljm.spring_boot.repository;
+
+import com.ljm.spring_boot.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
